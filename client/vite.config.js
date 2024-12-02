e4912b10c6122f8e2feb5ef844faef5caa8db30e
jorgeatlolasux <jorgeplasenciaaj@gmail.com>
2024-12-02 14:16:15 -0700

    aver

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
   plugins: [react()],
-})
+});
